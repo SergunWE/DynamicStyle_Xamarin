badd514f6815d76a5fdcd1bed5950d54f574982c
Sergei Shubenkov <sergunwe152@gmail.com>
2022-09-09 18:42:00 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UpText_Clicked(object sender, EventArgs e)
+        {
+            DynamicStyleHelper.ChangeFontSize(1);
+        }
+
+        private void DownText_Clicked(object sender, EventArgs e)
+        {
+            DynamicStyleHelper.ChangeFontSize(-1);
+        }
+
+        private void UpPadding_Clicked(object sender, EventArgs e)
+        {
+            DynamicStyleHelper.ChangeThickness(1);
+        }
+
+        private void DownPadding_Clicked(object sender, EventArgs e)
+        {
+            DynamicStyleHelper.ChangeThickness(-1);
+        }
+
+        private void ThemeChange_Clicked(object sender, EventArgs e)
+        {
+            switch (Application.Current.RequestedTheme)
+            {
+                case OSAppTheme.Light:
+                    Application.Current.UserAppTheme = OSAppTheme.Dark;
+                    break;
+                case OSAppTheme.Dark:
+                    Application.Current.UserAppTheme = OSAppTheme.Light;
+                    break;
+            }
+        }
+    }
+}
